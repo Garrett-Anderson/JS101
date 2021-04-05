7181b6233f3a784a3adf3aab391ebab6c89509f5
Garrett-Anderson <77418386+Garrett-Anderson@users.noreply.github.com>
2021-04-05 11:47:30 -0500

    rps walkthrough

---
+++ lesson2/rps.js
@@ -0,0 +1,46 @@
+// ask the user for their move
+// the computer will choose their move 
+// display who won/ show result
+
+const readline = require('readline-sync');
+VALID_CHOICES = ['rock', 'paper', 'scissors'];
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice.");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
+
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+      (choice === 'paper' && computerChoice === 'rock') ||
+      (choice === 'scissors' && computerChoice === 'paper')) {
+    prompt('You win!');
+  }  else if ((choice === 'rock' && computerChoice === 'paper') ||
+              (choice === 'paper' && computerChoice === 'scissors') ||
+              (choice === 'scissors' && computerChoice === 'rock')) {
+    prompt('Computer wins!');
+  } else {
+    prompt("It's a tie");
+  }
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+}
