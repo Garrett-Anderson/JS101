731e9a92c0cac47023d48ea5087ff70f582c9c34
Garrett-Anderson <77418386+Garrett-Anderson@users.noreply.github.com>
2021-06-16 14:10:50 -0500

    rps change

--- lesson2/rps.js
+++ lesson2/rps.js
@@ -3,40 +3,58 @@
 // display who won/ show result
 
 const readline = require('readline-sync');
-VALID_CHOICES = ['rock', 'paper', 'scissors'];
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
+
+
+function roundWinner(userChoice, computerChoice) {
+  return WINNING_COMBOS[userChoice].includes(computerChoice);
+}
+
+function displayWinner(userChoice, computerChoice) {
+  prompt(`you chose ${userChoice}, computer chose ${computerChoice}`)
+
+  if (roundWinner(userChoice, computerChoice)) {
+    prompt("User wins this round!\n");
+  } else if (userChoice === computerChoice) {
+    prompt("It's a tie\n");
+  } else {
+    prompt("Computer wins this round!\n");
+  }
+}
+
+
 
 while (true) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question();
 
   while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice.");
+    prompt("That's not a valid choice. Guess again!");
     choice = readline.question();
   }
 
+  let userChoice = VALID_CHOICES
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
-
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
-    prompt('You win!');
-  }  else if ((choice === 'rock' && computerChoice === 'paper') ||
-              (choice === 'paper' && computerChoice === 'scissors') ||
-              (choice === 'scissors' && computerChoice === 'rock')) {
-    prompt('Computer wins!');
-  } else {
-    prompt("It's a tie");
-  }
+  roundWinner(userChoice, computerChoice);
+  displayWinner(choice, computerChoice);
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
+
   while (answer[0] !== 'n' && answer[0] !== 'y') {
     prompt('Please enter "y" or "n".');
     answer = readline.question().toLowerCase();
