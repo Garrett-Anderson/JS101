6ac939a904a2f588853f1cdfe0fc6f750642dfe7
Garrett-Anderson <77418386+Garrett-Anderson@users.noreply.github.com>
2021-06-24 17:12:14 -0500

    exercise easy 2 finished

---
+++ exercises/easy_2/elleven.js
@@ -0,0 +1,48 @@
+function stringToInteger(string) {
+  let numberArray = string.split('');
+  let length = numberArray.length;
+  return numberArray.reduce((sum, number, index) => {
+    return sum + (number * (10 ** (length - index - 1)));
+  }, 0);
+}
+
+console.log(stringToInteger("4321") === 4321); // logs true
+console.log(stringToInteger("570") === 570); // logs true
+
+function stringToSignedInteger(string) {
+  switch (string[0]) {
+    case "-":
+      return -stringToInteger(string.slice(1, string.length));
+    case "+":
+      return stringToInteger(string.slice(1, string.length));
+    default:
+      return stringToInteger(string);
+  }
+}
+
+
+
+console.log(stringToSignedInteger("4321") === 4321); // logs true
+console.log(stringToSignedInteger("-570") === -570); // logs true
+console.log(stringToSignedInteger("+100") === 100); // logs true
+
+
+const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function integerToString(number) {
+  let result = '';
+
+  do {
+    let remainder = number % 10;
+    number = Math.floor(number / 10);
+
+    result = DIGITS[remainder] + result;
+  } while (number > 0);
+
+  return result;
+}
+
+integerToString(4321);        // "4321"
+integerToString(0);           // "0"
+integerToString(5000);        // "5000"
+integerToString(1234567890);  // "1234567890"
